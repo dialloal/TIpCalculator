24ef4625acbde0a9bdb015a0854fca0575593773
Alhassan Diallo <dialloal@msu.edu>
2020-09-20 23:05:58 -0400

    Initial Commit

---
+++ TIpCalculator/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  TIpCalculator
+//
+//  Created by user181030 on 9/20/20.
+//  Copyright © 2020 CodePath. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
